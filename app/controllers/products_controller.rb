2dbc6616fcbf57a06cb3be31dd3bb64ec4bcde74
Viktor <viktor.ivanov@extrawest.com>
2022-09-15 15:59:39 +0300

    Added bootstrap styles. Configurated navigation between pages.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,24 @@
 class ProductsController < ApplicationController
   def index
-    
+    @products = Product.all
+  end
+
+  def new
+
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    @product.save
+    redirect_to @product
+  end
+
+  private def product_params
+      params.require(:product).permit(:title, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root "products#index"
+  root "products#index", as: "home"
 
-  get "about" => "pages#about"
+  get "about" => "pages#about", as: "about"
+  # get "products/new" => "products#new"
+
+  resources :products
 end
